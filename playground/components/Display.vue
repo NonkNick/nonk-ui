05f1c16f8cb3860a2012ef64aeb5a436330f137f
Nick Riemersma <pieterstienstra@hotmail.com>
2025-12-09 15:31:14 +0100

    error quickfix

--- playground/components/Display.vue
+++ playground/components/Display.vue
@@ -1,11 +1,13 @@
 <template>
   <ButtonDemo/>
   <ButtonGroupDemo/>
+  <PaginationDemo/>
 </template>
 
 <script setup lang="ts">
 import ButtonDemo from "./ButtonDemo.vue";
 import ButtonGroupDemo from "./ButtonGroupDemo.vue";
+import PaginationDemo from "./PaginationDemo.vue";
 </script>
 
 <style scoped>
